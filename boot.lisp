597b7e4c5090339e89244c88f9879f66fd0f1a95
Lukáš Hozda <luk.hozda@gmail.com>
2026-08-26 23:03:53 +0200

    add native world boot loader

---
+++ boot.lisp
@@ -0,0 +1,21 @@
+;;;; Native console boot: load the world and leave a LispBSD listener.
+
+(require "asdf")
+
+(let* ((root (make-pathname :name nil :type nil :defaults *load-truename*))
+       (asd (merge-pathnames "lispbsd.asd" root)))
+  (asdf:initialize-source-registry
+   `(:source-registry (:directory ,root) :ignore-inherited-configuration))
+  (asdf:load-asd asd)
+  (asdf:load-system :lispbsd))
+
+(in-package #:lispbsd)
+
+(unless (and *world* (eq (world-phase *world*) ':operational))
+  (make-hosted-world :name "lispbsd"))
+
+(setf *package* (find-package '#:lispbsd))
+(format t "~%LispBSD ~A~%world ~A  phase ~S~%~%"
+        (lisp-implementation-version)
+        (world-id *world*)
+        (world-phase *world*))
